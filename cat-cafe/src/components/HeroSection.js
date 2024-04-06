8ec9d8f4de07e2574648635352d2773f0f03bbd3
BubuCatt <jiayuhsu8240@gmail.com>
2024-04-06 02:54:45 -0700

    fixed the herosection

--- cat-cafe/src/components/HeroSection.js
+++ cat-cafe/src/components/HeroSection.js
@@ -9,8 +9,8 @@ function HeroSection(){
         <div className="hero-section" style={{ backgroundImage: `url(${heroImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
         <Container>
           <Row className="justify-content-center align-items-center" style={{ minHeight: '80vh' }}>
-            <Col md={8} className="text-center " style={{ color: 'black' }}>
-              <h1 >Welcome to our Cat Cafe</h1>
+            <Col md={8} className="text-center " style={{ color: 'dark-gray' }}>
+              <h1 >Welcome to Meow Match Cafe</h1>
               <p>Discover a cozy and relaxing atmosphere where you can enjoy delicious drinks and spend time with adorable cats.</p>
               <Button variant="light" size="lg">Explore Meow Cafe</Button>
             </Col>
