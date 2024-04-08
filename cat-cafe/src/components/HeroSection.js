53fbe6077166156524fb82e0fd29314a51977de6
BubuCatt <jiayuhsu8240@gmail.com>
2024-04-08 00:59:39 -0700

    updated homepage css

--- cat-cafe/src/components/HeroSection.js
+++ cat-cafe/src/components/HeroSection.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import heroImage from '../assets/hero-image.jpg'; 
+import "../styles/home.css";
 
-function HeroSection(){
+
+const HeroSection =()=>{
+
+  const handleClick = () => {
+    window.scrollTo({
+      top: window.innerHeight, // Scroll down by the height of the viewport
+      behavior: 'smooth' // Smooth scroll
+    });
+  }
+
 
     return(
 
@@ -12,12 +22,14 @@ function HeroSection(){
             <Col md={8} className="text-center " style={{ color: 'dark-gray' }}>
               <h1 >Welcome to Meow Match Cafe</h1>
               <p>Discover a cozy and relaxing atmosphere where you can enjoy delicious drinks and spend time with adorable cats.</p>
-              <Button variant="light" size="lg">Explore Meow Cafe</Button>
+              <Button variant="light" size="lg" onClick={handleClick} className='btn-custom'>Explore Meow Cafe</Button>
             </Col>
           </Row>
         </Container>
+       
       </div>
 
     )
+}
 
-}export default HeroSection;
+export default HeroSection;
